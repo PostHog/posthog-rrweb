8272d6d2c11bccb6c2b3ce6a9123788169686daf
Paul D'Ambra <paul@posthog.com>
2025-01-30 13:12:36 +0000

    try this

---
+++ scripts/npm-release.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# we don't use all of the packages so we don't publish them all
+
+# Define the package paths
+# assumes this is run with cwd as the root of the repo
+  PACKAGES=(
+    "./packages/record"
+    "./packages/plugins/rrweb-plugin-console-record"
+    "./packages/types"
+    "./packages/rrweb"
+  )
+
+yarn run build:all
+
+# first as a dry run to check they should all publish
+for PACKAGE in "${PACKAGES[@]}"; do
+  echo "Publishing $PACKAGE"
+  npm publish  --dry-run --workspace "$PACKAGE" || { echo "Failed to publish $PACKAGE"; exit 1; }
+done
+
+# then for real
+#for PACKAGE in "${PACKAGES[@]}"; do
+#  echo "Publishing $PACKAGE"
+#  npm publish --workspace "$PACKAGE" || { echo "Failed to publish $PACKAGE"; exit 1; }
+#done
