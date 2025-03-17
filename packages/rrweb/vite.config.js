7927188d96484e83d10ba901041bada81b2b043f
pauldambra <paul@posthog.com>
2025-03-17 15:43:32 +0000

    chore: remove rollup web worker processing

--- packages/rrweb/vite.config.js
+++ packages/rrweb/vite.config.js
@@ -1,4 +1,17 @@
 import config from '../../vite.config.default';
 
 // export default config('src/index.ts', 'rrweb', { outputDir: 'dist/main' });
-export default config('src/index.ts', 'rrweb');
+export default config('src/index.ts', 'rrweb', {
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name?.endsWith('.map')) {
+                        return '[name]';
+                    }
+                    return 'assets/[name]-[hash][extname]';
+                },
+            },
+        },
+    },
+});
